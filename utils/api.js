3fd6c9728ae7ecfb104c272f3bc062c61ff81b47
Pedro Mendonca <peumendonca@gmail.com>
2019-03-22 17:07:27 +0000

    Formating

--- utils/api.js
+++ utils/api.js
@@ -4,18 +4,18 @@ const { log, error } = console
 
 export async function fetchPoliceRecords(lat, lon) {
     try {
-        const resp = await get(`https://data.police.uk/api/crimes-at-location?lat=${lat}&lng=${lon}`)
+        const resp = await get(`https://data.police.uk/api/crimes-at-location?lat=${lat}&lng=${lon}&date=2019-03`)
         return resp.data
     } catch (e) {
         error(e)
         return []
     }
 }
- 
+
 export async function fetchPostcodeInfo(postcode) {
     try {
         const resp = await get(`http://api.postcodes.io/postcodes/${postcode}`)
-        log(resp)
+        log('Postcode found')
         return resp.data.result
     } catch (e) {
         return null
